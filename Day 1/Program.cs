2e4cd614a8f5c4fc17eee709e5674ae487907ee7
Chase Hanson <chase@iactionable.com>
2025-12-06 16:59:47 -0800

    Day 1

--- Day 1/Program.cs
+++ Day 1/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using var fs = File.Open("input.txt", FileMode.Open);
+using var sr = new StreamReader(fs);
+
+var position = 50;
+var exactCount = 0;
+var crossCount = 0;
+
+/*5501 > 6047*/
+
+while (!sr.EndOfStream)
+{
+    var line = await sr.ReadLineAsync();
+    var oldPosition = position;
+    position += (line![0] == 'L' ? -1 : 1) * int.Parse(line[1..]);
+
+    crossCount += TimesCrossing0(oldPosition, position);
+
+    position %= 100;
+
+    if (position < 0)
+    {
+        position += 100;
+    }
+
+    if (position == 0)
+    {
+        exactCount++;
+    }
+}
+
+int TimesCrossing0(int oldPosition, int newPosition)
+{
+    var crossCount = 0;
+    if ((oldPosition != 0 && int.IsNegative(newPosition)) || newPosition == 0)
+    {
+        crossCount++;
+    }
+    crossCount += Math.Abs(newPosition / 100);
+    return crossCount;
+}
+
+Console.WriteLine($"ExactCount: {exactCount} - CrossCount: {crossCount}");
